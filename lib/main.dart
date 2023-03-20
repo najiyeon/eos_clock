019051c15368b40ed33b5fd507766f8ff4cfece2
2021038122 <2021038122@hanyang.ac.kr>
2023-03-20 20:09:11 +0900

    week3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:eos_clock/screens/my_home_page.dart';
+import 'package:eos_clock/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.lightGreen, fontFamily: 'Exo'
       ),
-      home: MyHomePage()
+      home: HomeScreen()
     );
   }
 }
